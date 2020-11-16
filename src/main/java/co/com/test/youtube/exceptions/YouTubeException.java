5b8886188e7c4a86ce71af48dc529c707fd7a9d9
Juan Francisco Builes Montoya <pacho0323@hotmail.com>
2020-11-15 19:47:52 -0500

    Se corrige la exception

--- src/main/java/co/com/test/youtube/exceptions/YouTubeException.java
+++ src/main/java/co/com/test/youtube/exceptions/YouTubeException.java
@@ -2,7 +2,7 @@ package co.com.test.youtube.exceptions;
 
 @SuppressWarnings("serial")
 public class YouTubeException extends AssertionError {
-	public static final String NOT_FOUND_VIDEO = "VIDEO DIDN´T HAVE THE SPECIFIED  DURATION ";
+	public static final String NOT_FOUND_VIDEO = "VIDEO DID NOT HAVE THE SPECIFIED  DURATION ";
 
 	public YouTubeException(String message, Throwable cause) {
 		super(message, cause);
