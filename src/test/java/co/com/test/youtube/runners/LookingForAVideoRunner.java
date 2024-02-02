0673fc0fd485a0a16efb2d07cf28ebac170da36f
Juan Francisco Builes Montoya <juanbuiles00@users.noreply.github.com>
2024-02-02 09:45:51 -0500

    Update LookingForAVideoRunner.java

--- src/test/java/co/com/test/youtube/runners/LookingForAVideoRunner.java
+++ src/test/java/co/com/test/youtube/runners/LookingForAVideoRunner.java
@@ -7,10 +7,10 @@ import org.junit.platform.suite.api.Suite;
 
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
-@Suite
-@IncludeEngines("cucumber")
-@SelectClasspathResource("/features")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "io.cucumber.core.plugin.SerenityReporterParallel,pretty,timeline:build/test-results/timeline")
-@ConfigurationParameter(key= "cucumber.features", value="src/test/resources/features/look_video.feature")
-public class LookingForAVideoRunner {
-}
+//@Suite
+//@IncludeEngines("cucumber")
+//@SelectClasspathResource("/features")
+//@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "io.cucumber.core.plugin.SerenityReporterParallel,pretty,timeline:build/test-results/timeline")
+//@ConfigurationParameter(key= "cucumber.features", value="src/test/resources/features/look_video.feature")
+//public class LookingForAVideoRunner {
+//}
